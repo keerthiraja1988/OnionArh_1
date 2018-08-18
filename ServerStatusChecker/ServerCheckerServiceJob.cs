2e5e9146a4afede90df341c759aeb4d9c956093d
keerthiraja1988 <keerthiraja1988@gmail.com>
2018-08-18 15:43:42 +0530

    1st Commit
    1st Chnages checked into github

---
+++ ServerStatusChecker/ServerCheckerServiceJob.cs
@@ -0,0 +1,33 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerStatusChecker
+{
+    public class ServerCheckerServiceJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+
+        {
+
+            Debug.WriteLine("Service Checker Started");
+        }      
+
+    }
+
+    public class ServerCheckerServiceJob1 : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+
+        {
+
+            Debug.WriteLine("Service Checker Started");
+        }
+
+    }
+}
